8428d0e3e94f635f39c0df8da2530eb96f18bf91
BigBoPeep420 <236509553+BigBoPeep420@users.noreply.github.com>
2026-01-09 16:15:39 -0600

    Finished rewriting all javascript
    Calculator was moved to calc.js and gets imported to index.js
        where all the page logic lives.

--- scripts/calc.js
+++ scripts/calc.js
@@ -41,6 +41,7 @@ class Calculator{
         this.activeInput = 'iA'; this.updateDisplay();
     };
     delete() {
+        if(this.res !== null) this.clear();
         if(this[this.activeInput] == ''){
             if(this.activeInput == 'iB') this.activeInput = 'iOp'
             else if(this.activeInput == 'iOp') this.activeInput = 'iA';
@@ -55,33 +56,47 @@ class Calculator{
     };
     input(action) {
         const isNum = !isNaN(action) || action == '.';
-        const isOp = ['+', '-', '*', '/', 'fnc1', 'fnc2', 'fnc3'].contains(action);
-        if(action == 'fncEdit'){
-            this.onFn(true);
-            return;
+        const isOp = ['+', '-', '*', '/', 'fnc1', 'fnc2', 'fnc3'].includes(action);
+        switch(action){
+            case '=':
+                this.calculate();
+                return;
+            case 'fncEdit':
+                this.onFn(true);
+                return;
+            case 'del':
+                this.delete();
+                return;
+            case 'clr':
+                this.clear();
+                return;
+            default:
+                if(this.res !== null && isOp){
+                    this.iA = this.res.toString();
+                    this.res = null;
+                    this.iOp = action;
+                    this.activeInput = 'iB';
+                    this.updateDisplay();
+                    return;
+                }
+                if(this.res !== null && isNum) this.clear();
+                if(isOp){
+                    if (this.iA == '') this.iA = '0';
+                    this.iOp = action;
+                    this.activeInput = 'iB';
+                } else {
+                    if(action == '.'){
+                        if(this[this.activeInput] == '') this[this.activeInput] = '0';
+                        if(this[this.activeInput].includes('.')) return;
+                    }
+                    if(this.activeInput == 'iOp') this.activeInput = 'iB';
+                    this[this.activeInput] += action;
+                }
+                this.updateDisplay();
         }
-        if(this.res !== null && isOp){
-            this.iA = this.result.toString();
-            this.res = null;
-            this.iOp = action;
-            this.activeInput = 'iB';
-            this.updateDisplay();
-            return;
-        }
-        if(this.res !== null && isNum) this.clear();
-        if(isOp){
-            if (this.iA == '') this.iA = '0';
-            this.iOp = action;
-            this.activeInput = 'iB';
-        } else {
-            if(action == '.'){
-                if(this[this.activeInput] == '') this[this.activeInput] = '0';
-                if(this[this.activeInput].contains('.')) return;
-            }
-            if(this.activeInput == 'iOp') this.activeInput = 'iB';
-            this[this.activeInput] = action;
-        }
-        this.updateDisplay();
+        
+        
+        
     };
     calculate() {
         if(this.iA == '' || this.iB == '' || this.iOp == '') return;
@@ -89,7 +104,7 @@ class Calculator{
         const b = parseFloat(this.iB);
         let result = 0;
         if(this.iOp in this.methods) {
-            result = this.methods[this.iOp];
+            result = this.methods[this.iOp](a, b);
             if(typeof result == 'number') result = Math.round(result * 10000000) / 10000000;
             this.res = result;
             this.updateDisplay();
@@ -104,15 +119,30 @@ class Calculator{
     };
     updateDisplay(str){
         let txt = '';
-        if(this.res !== null){
-            if(this.res == Infinity){
+        if(str){
+            txt = str;
+        }else if(this.res !== null){
+            if(this.res == Infinity || isNaN(this.res)){
                 txt = '8008135';
             } else txt = this.res;
-        } else if(this.activeInput == 'iB' && this.iB !== ''){
+        }else if(this.activeInput == 'iB' && this.iB !== ''){
             txt = this.iB;
-        } else if(this.iOp !== '') {
-            txt = this.iOp;
-        } else txt = this.iA || '0';
+        }else if(this.iOp !== '') {
+            switch(this.iOp){
+                case 'fnc1':
+                    txt = 'F1';
+                    break;
+                case 'fnc2':
+                    txt = 'F2';
+                    break;
+                case 'fnc3':
+                    txt = 'F3';
+                    break;
+                default:
+                    txt = this.iOp;
+                    break;
+            }
+        }else txt = this.iA || '0';
         this.display.textContent = txt;
     };
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,114 @@
-import { act } from "react";
-import { Calculator } from "./calc";
+import { Calculator } from "./calc.js";
 
 function showFncEdit(bShow){
     if(bShow){
         document.getElementById('fncSelect').dispatchEvent(new Event('change'));
-        document.getElementById('fncEdit').classList.add('show');
+        fncEditModal.classList.add('show');
     } else fncEditModal.classList.remove('show');
 };
+function showAlert(txt, icon = '⁉', duration = 5000){
+    const alertModal = document.getElementById('alert');
+    alertModal.querySelector('#alertText').textContent = txt;
+    alertModal.querySelector('#alertIcon').textContent = icon;
+    if(alertTimeout) clearTimeout(alertTimeout);
+    alertModal.classList.add('show');
+    alertTimeout = setTimeout(() => {hideAlert();}, duration);
+}
+function hideAlert(){
+    document.getElementById('alert').classList.remove('show');
+    alertTimeout = null;
+}
+function pressButton(buttonStr){
+    let btn;
+    switch(buttonStr){
+        
+    }
+}
 
 
 
 const keyboardMap = {
-    '0': () => kowlkulater.input('0'),
-    '1': () => kowlkulater.input('1'),
-    '2': () => kowlkulater.input('2'),
-    '3': () => kowlkulater.input('3'),
-    '4': () => kowlkulater.input('4'),
-    '5': () => kowlkulater.input('5'),
-    '6': () => kowlkulater.input('6'),
-    '7': () => kowlkulater.input('7'),
-    '8': () => kowlkulater.input('8'),
-    '9': () => kowlkulater.input('9'),
-    'Enter': () => kowlkulater.calculate(),
-    '=': () => kowlkulater.calculate(),
-    '+': () => kowlkulater.input('+'),
-    '-': () => kowlkulater.input('-'),
-    '*': () => kowlkulater.input('*'),
-    '/': () => kowlkulater.input('/'),
-    '.': () => kowlkulater.input('.'),
-    'Backspace': () => kowlkulater.delete(),
-    'Delete': () => kowlkulater.delete(),
-    'SBackspace': () => kowlkulater.clear(),
+    '0': '0', '1': '1', '2': '2', '3': '3', '4': '4', '5': '5',
+    '6': '6', '7': '7', '8': '8', '9': '9', 'Enter': '=', '=': '=',
+    '+': '+', '-': '-', '*': '*', '/': '/', '.': '.', 'Backspace': 'del',
+    'Delete': 'del', 'SBackspace': 'clr',
 }
 const kowlkulater = new Calculator(document.getElementById('display'), showFncEdit);
+const fncEditModal = document.getElementById('fncEdit');
+let alertTimeout = null;
 
+window.addEventListener('click', e => {
+    if(fncEditModal.contains(e.target)) {}
+    else fncEditModal.classList.remove('show');
+})
 document.getElementById('buttons').addEventListener('click', e => {
+    if(fncEditModal.classList.contains('show')) fncEditModal.classList.remove('show');
     if(!e.target.classList.contains('key')) return;
     kowlkulater.input(e.target.dataset.action);
     e.stopPropagation();
 })
 document.getElementById('fncClose').addEventListener('click', e => {
     showFncEdit(false);
+})
+document.getElementById('fncConfirm').addEventListener('click', e => {
+    const fncSlot = document.getElementById('fncSelect').value;
+    const expr = document.getElementById('fncExpression').value;
+    const newMethod = new Function('a', 'b', `return ${expr};`);
+    const success = kowlkulater.registerMethod(fncSlot, newMethod);
+    let slot = 'F1';
+    switch(fncSlot){
+        case 'fnc1':
+            slot = 'F1';
+            break;
+        case 'fnc2':
+            slot = 'F2';
+            break;
+        case 'fnc3':
+            slot = 'F3';
+            break;
+    }
+    if(success){
+        showAlert(`Custom Function Saved to ${slot}!`, '✔');
+        fncEditModal.classList.remove('show');
+    } else {
+        showAlert('Custom Function Not Valid!', '🛑');
+    }
+})
+document.getElementById('fncSelect').addEventListener('change', e => {
+    document.getElementById('fncExpression').value = kowlkulater.getMethodString(e.target.value);
+})
+document.getElementById('alertDismiss').addEventListener('click', e => {
+    hideAlert();
+})
+window.addEventListener('keydown', e => {
+    if(document.activeElement.tagName == 'INPUT' ||
+        document.activeElement.tagName == 'TEXTAREA') return;
+    let key = e.key;
+    if(key == 'Backspace' && e.shiftKey) key = 'SBackspace';
+    const action = keyboardMap[key] || e.key;
+    const btn = document.querySelector(`.key[data-action="${action}"]`);
+    if (btn){
+        e.preventDefault();
+        btn.classList.add('active');
+        kowlkulater.input(action);
+    }
+})
+window.addEventListener('keyup', e => {
+    let key = e.key;
+    if(key == 'Backspace' && e.shiftKey) key = 'SBackspace';
+    const action = keyboardMap[key] || e.key;
+    const btn = document.querySelector(`.key[data-action="${action}"]`);
+    if(btn) btn.classList.remove('active');
+})
+document.getElementById('testExpression').addEventListener('click', e => {
+    const exp = document.getElementById('fncExpression').value;
+    try{
+        const fnc = new Function('a', 'b', `return ${exp};`);
+        fnc(2, 5);
+        document.getElementById('testExpression').classList.add('pass');
+        document.getElementById('testExpression').classList.remove('fail');
+    }catch{
+        document.getElementById('testExpression').classList.remove('pass');
+        document.getElementById('testExpression').classList.add('fail');
+    }
 })
