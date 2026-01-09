52b4177623d84d9a201e0bc01ef2d3c0bcffa877
BigBoPeep420 <236509553+BigBoPeep420@users.noreply.github.com>
2026-01-09 12:31:02 -0600

    Rewrote Calculator class and restructured
    Started moving all input logic into index.js and importing
        the Calculator class from calc.js

--- README.md
+++ README.md
@@ -2,5 +2,3 @@
 My calculator. There are many like it, but this one is mine.
 
 
-
-TODO: Add test button feature to custom expression modal
--- scripts/calc.js
+++ scripts/calc.js
@@ -1,239 +1,119 @@
-const keys = document.querySelector('.buttons');
-const display = document.querySelector('.display');
-const fncEditModal = document.querySelector('#fncEdit');
-const fncEditClose = document.querySelector('#fncClose');
-const fncEditConfirm = document.querySelector('#fncConfirm');
-const fncDropdown = document.querySelector('#fncSelect');
-const fncExpression = document.querySelector('#fncExpression');
-const alertModal = document.querySelector('#alert');
-const testExpBtn = document.querySelector('#testExpression');
-let alertTimeout = null;
 
 class Calculator{
-    constructor(displayDiv) {
-        this.inputA = '';
-        this.inputB = '';
-        this.op = '';
-        this.display = displayDiv;
+    constructor(displayDiv, onFnCallback){
+        this.iA = '';
+        this.iB = '';
+        this.iOp = '';
+        this.res = '';
+        this.onFn = onFnCallback;
+        this.activeInput = 'iA';
+        if(!displayDiv) this.display = document.querySelector('.display');
+        else this.display = displayDiv;
 
         this.methods = {
-        "+": (a, b) => a + b,
-        "-": (a, b) => a - b,
-        "*": (a, b) => a * b,
-        "/": (a, b) => a / b,
+            '+': (a,b) => a+b,
+            '-': (a,b) => a-b,
+            '*': (a,b) => a*b,
+            '/': (a,b) => a/b,
         };
     };
 
-    registerMethod(sym, fn) {
+    registerMethod(id, fn) {
         try{
             fn(1,1);
-            this.methods[sym] = fn;
-        } catch{}
-    };
-    areInputsEmpty() {
-        return { inputA: !this.inputA, inputB: !this.inputB, op: !this.op };
-    };
-    updateDisplay() {
-        const MAX_CHARS = 14;
-        let txt = this.inputB || this.inputA || '0';
-        if(this.op && !this.inputB) txt = this.inputA;
-        if(txt.length > MAX_CHARS) txt = '..' + txt.slice(-(MAX_CHARS - 2));
-        if (txt.startsWith('.')) txt = 0 + txt;
-        if (txt == 'Infinity') txt = '800815';
-        this.display.textContent = txt;
-    }
-    inputNum(str) {
-        const empties = this.areInputsEmpty();
-        if(empties.op) {
-            if(str == '.' && this.inputA.endsWith('.')) return;
-            this.inputA += str;
-            this.updateDisplay();
-        } else {
-            const lastChar = this.inputB.charAt(this.inputB.length - 1);
-            if(str == '.' && this.inputB.endsWith('.')) return;
-            this.inputB += str;
-            this.updateDisplay();
+            this.methods[id] = fn;
+            return true;
+        } catch{
+            return false;
         }
     };
-    inputOp(str) {
-        const empties = this.areInputsEmpty();
-        if (empties.inputA) return;
-        if(!empties.inputA && !empties.op && !empties.inputB) {
-            this.calculate();
-            this.op = str;
-        } else {
-            this.op = str;
-            this.updateDisplay();
-        }
-        
-    };
-    calculate() {
-        if(!this.inputA || !this.op || !this.inputB) return;
-        const numA = parseFloat(this.inputA) || 0;
-        const numB = parseFloat(this.inputB) || 0;
-        if(this.op in this.methods) {
-            let result = this.methods[this.op](numA, numB);
-            if(!Number.isFinite(result)){
-                this.clear();
-                this.display.textContent = "Error";
-            }
-            result = Math.round(result * 1000000000) / 1000000000;
-            this.inputA = result.toString();
-            this.inputB = ''; this.op = '';
-            this.updateDisplay();
-            return result;
-        }else {
-            this.inputA = ''; this.inputB =''; this.op = '';
-            this.display.textContent = "No Fn";
-        }
-    };
-    clear() {
-        this.inputA = ''; this.inputB = ''; this.op = '';
-        this.display.textContent = '0';
-    };
-    delete() {
-        const empties = this.areInputsEmpty();
-        if(!empties.inputB) {
-            this.inputB = this.inputB.slice(0, -1);
-            if(this.inputB == '-') this.inputB = '';
-        }else if(!empties.op) {
-            this.op = '';
-        }else if(!empties.inputA) {
-            this.inputA = this.inputA.slice(0, -1);
-            if(this.inputA == '-') this.inputA = '';
-        }
-        this.updateDisplay();
-    };
-    getMethodString(key) {
-        if(!(key in this.methods)) return '';
-        let str = this.methods[key].toString();
-        str = str.trim();
-        str = str.replaceAll('function anonymous(a,b\n)', '');
+    getMethodString(id){
+        if(!(id in this.methods)) return '';
+        let str = this.methods[id].toString();
+        str = str.trim().replaceAll('function anonymous(a,b\n)', '');
         str = str.replaceAll(' {\n', '');
         str = str.replaceAll('return ', '');
         str = str.replaceAll(';\n}', '');
         return str;
     };
-};
-
-function showAlert(txt, icon = "⁉", duration = 5000){
-    const alertModal = document.getElementById('alert');
-    alertModal.querySelector('#alertText').textContent = txt;
-    alertModal.querySelector('#alertIcon').textContent = icon;
-    if(alertTimeout) {clearTimeout(alertTimeout)};
-    alertModal.classList.add('show');
-    alertTimeout = setTimeout(() => {hideAlert();}, duration);
-}
-function hideAlert(){
-    document.getElementById('alert').classList.remove('show');
-    alertTimeout = null;
-}
-
-
-const kowlkulater = new Calculator(display);
-const keyboardInput = {
-    '0': () => kowlkulater.inputNum('0'),
-    '1': () => kowlkulater.inputNum('1'),
-    '2': () => kowlkulater.inputNum('2'),
-    '3': () => kowlkulater.inputNum('3'),
-    '4': () => kowlkulater.inputNum('4'),
-    '5': () => kowlkulater.inputNum('5'),
-    '6': () => kowlkulater.inputNum('6'),
-    '7': () => kowlkulater.inputNum('7'),
-    '8': () => kowlkulater.inputNum('8'),
-    '9': () => kowlkulater.inputNum('9'),
-    'Enter': () => kowlkulater.calculate(),
-    '+': () => kowlkulater.inputOp('+'),
-    '-': () => kowlkulater.inputOp('-'),
-    '*': () => kowlkulater.inputOp('*'),
-    '/': () => kowlkulater.inputOp('/'),
-    '.': () => kowlkulater.inputNum('.'),
-    'Backspace': () => kowlkulater.delete(),
-    'SBackspace': () => kowlkulater.clear(),
-};
-
-
-keys.addEventListener('click', e => {
-    const target = e.target;
-    const action = e.target.dataset.action;
-    const content = e.target.textContent;
-
-    if(!target.classList.contains('key')) return;
-    switch(action){
-        case 'clear': 
-            kowlkulater.clear();
-            break;
-        case 'del':
-            kowlkulater.delete();
-            break;
-        case 'equal':
-            kowlkulater.calculate();
-            break;
-        case 'fncEdit':
-            e.stopPropagation();
-            fncDropdown.dispatchEvent(new Event('change'));
-            fncEditModal.classList.add('show');
-            break;
-        case 'fnc1':
-        case 'fnc2':
-        case 'fnc3':
-            kowlkulater.inputOp(action);
-            break;
-        default:
-            if(target.classList.contains('op')){
-                kowlkulater.inputOp(action);
-            }else if(target.classList.contains('num')){
-                kowlkulater.inputNum(action);
+    clear() {
+        this.iA = ''; this.iB = ''; this.iOp = ''; this.res = null;
+        this.activeInput = 'iA'; this.updateDisplay();
+    };
+    delete() {
+        if(this[this.activeInput] == ''){
+            if(this.activeInput == 'iB') this.activeInput = 'iOp'
+            else if(this.activeInput == 'iOp') this.activeInput = 'iA';
+        }
+        if(this.activeInput == 'iOp'){
+            this.iOp = '';
+            this.activeInput = 'iA';
+        } else {
+            this[this.activeInput] = this[this.activeInput].slice(0, -1);
+        }
+        this.updateDisplay();
+    };
+    input(action) {
+        const isNum = !isNaN(action) || action == '.';
+        const isOp = ['+', '-', '*', '/', 'fnc1', 'fnc2', 'fnc3'].contains(action);
+        if(this.res !== null && isOp){
+            this.iA = this.result.toString();
+            this.res = null;
+            this.iOp = action;
+            this.activeInput = 'iB';
+            this.updateDisplay();
+            return;
+        }
+        if(this.res !== null && isNum) this.clear();
+        if(isOp){
+            if (this.iA == '') this.iA = '0';
+            this.iOp = action;
+            this.activeInput = 'iB';
+        } else {
+            if(action == '.'){
+                if(this[this.activeInput] == '') this[this.activeInput] = '0';
+                if(this[this.activeInput].contains('.')) return;
             }
-    }
-});
-fncEditClose.addEventListener('click', () => fncEditModal.classList.remove('show'));
-window.addEventListener('click', e => {
-    if(e.target === fncEditModal) {
-        fncEditModal.classList.remove('show');
+            if(this.activeInput == 'iOp') this.activeInput = 'iB';
+            this[this.activeInput] = action;
+        }
+        this.updateDisplay();
     };
-});
-fncEditConfirm.addEventListener('click', e => {
-    const selectedF = fncDropdown.value;
-    const custExpress = fncExpression.value;
-
-    try {
-        const newMethod = new Function('a', 'b', `return ${custExpress};`);
-        kowlkulater.registerMethod(selectedF, newMethod);
-        fncEditModal.classList.remove('show');
-        kowlkulater.methods[selectedF](1,1);
-
-        showAlert('Custom Function Saved!', '✔');
-    } catch (err) {
-        showAlert('Custom Function Not Valid!', '🛑');
+    calculate() {
+        if(this.iA == '' || this.iB == '' || this.iOp == '') return;
+        const a = parseFloat(this.iA);
+        const b = parseFloat(this.iB);
+        let result = 0;
+        if(this.iOp in this.methods) {
+            result = this.methods[this.iOp];
+            if(typeof result == 'number') result = Math.round(result * 10000000) / 10000000;
+            this.res = result;
+            this.updateDisplay();
+            this.iA = '';
+            this.iB = '';
+            this.iOp = '';
+            this.activeInput = 'iA';
+        } else { 
+            this.clear();
+            this.updateDisplay('No Func');
+        }
     };
-});
-fncDropdown.addEventListener('change', e => {
-    fncExpression.value = kowlkulater.getMethodString(e.target.value);
-});
-alertModal.addEventListener('click', (e) => {
-    if(e.target.id == 'alertDismiss') alertModal.classList.remove('show');
-});
-window.addEventListener('keydown', e => {
-    if(document.activeElement.tagName == 'INPUT' || 
-        document.activeElement.tagName == 'TEXTAREA') return;
-    const action = keyboardInput[e.key];
-    if(e.key == 'Backspace' && e.shiftKey){
-        keyboardInput['SBackspace']();
-    }else if(action){
-        e.preventDefault();
-        action();
+    updateDisplay(str){
+        let txt = '';
+        if(this.res !== null){
+            if(this.res == Infinity){
+                txt = '8008135';
+            } else txt = this.res;
+        } else if(this.activeInput == 'iB' && this.iB !== ''){
+            txt = this.iB;
+        } else if(this.iOp !== '') {
+            txt = this.iOp;
+        } else txt = this.iA || '0';
+        this.display.textContent = txt;
     };
-});
-testExpBtn.addEventListener('click', () => {
-    try{
-        const exp = new Function('a', 'b', `return ${fncExpression.value};`);
-        exp(2, 5);
-        document.querySelector('#testExpression').classList.add('pass');
-        document.querySelector('#testExpression').classList.remove('fail');
 
-    }catch{
-        document.querySelector('#testExpression').classList.remove('pass');
-        document.querySelector('#testExpression').classList.add('fail');
-    }
-})
+}
+
+
+
+export { Calculator };
